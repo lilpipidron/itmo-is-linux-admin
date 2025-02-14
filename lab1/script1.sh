c3e827cb98187e2267d863442d1a4bc03048b853
lilpipidron <lociX55lim@gmail.com>
2025-02-14 11:55:28 +0300

    fix

--- lab1/script1.sh
+++ lab1/script1.sh
@@ -8,7 +8,7 @@ while IFS=: read -r username _ uid _; do
 done < /etc/passwd
 
 #2
-sudo chage -l root | head -1 >> work3.log
+chage -l root | head -1 >> work3.log
 
 #3
 groups=""  
@@ -22,22 +22,22 @@ groups=${groups%,}
 echo groups >> work3.log
 
 #4
-sudo echo "Be careful!" > /etc/skel/readme.txt
+echo "Be careful!" > /etc/skel/readme.txt
 
 #5
-sudo useradd u1 -p $(openssl passwd -salt xx 12345678)
+useradd -m -p $(openssl passwd -salt xx 12345678) u1
 
 #6
 groupadd g1
 
 #7
-sudo usermod -aG g1 u1
+usermod -aG g1 u1
 
 #8
 id u1 >> work3.log
 
 #9
-sudo usermod -aG g1 myuser
+usermod -aG g1 myuser
 
 #10
 getent group g1 | cut -d: -f4 >> work3.log
@@ -46,39 +46,39 @@ getent group g1 | cut -d: -f4 >> work3.log
 usermod -s /usr/bin/mc u1
 
 #12
-sudo useradd u2 -p $(openssl passwd -salt xx 87654321)
+useradd -m -p $(openssl passwd -salt xx 87654321) u2
 
 #13
-sudo mkdir /home/test13
-sudo cp work3.log /home/test13/work3-1.log
-sudo cp work3.log /home/test13/work3-2.log
+mkdir /home/test13
+cp work3.log /home/test13/work3-1.log
+cp work3.log /home/test13/work3-2.log
 
 #14
-sudo groupadd test13_group
-sudo usermod -aG test13_group u1
-sudo usermod -aG test13_group u2
-sudo chown u1:test13_group /home/test13
-sudo chmod 750 /home/test13
-sudo chown u1:test13_group /home/test13/*
-sudo chmod 640 /home/test13/*
-sudo chmod a-x /home/test13/*
+groupadd test13_group
+usermod -aG test13_group u1
+usermod -aG test13_group u2
+chown u1:test13_group /home/test13
+chmod 750 /home/test13
+chown u1:test13_group /home/test13/*
+chmod 640 /home/test13/*
+chmod a-x /home/test13/*
 
 #15
-sudo mkdir /home/test14
-sudo chown u1:u1 /home/test14
-sudo chmod 1777 /home/test14
+mkdir /home/test14
+chown u1:u1 /home/test14
+chmod 1777 /home/test14
 
 #16
-sudo cp /bin/nano /home/test14/nano
-sudo chmod a+rx /home/test14/nano
-sudo chmod a+rw /home/test13/work3-1.log
-sudo chmod a+rw /home/test13/work3-2.log
+cp /bin/nano /home/test14/nano
+chmod a+rx /home/test14/nano
+chmod a+rw /home/test13/work3-1.log
+chmod a+rw /home/test13/work3-2.log
 
 #17
-sudo mkdir /home/test15
+mkdir /home/test15
 echo "123" >> /home/test15/secret_file
-sudo chmod 700 /home/test15
-sudo chmod 644 /home/test15/secret_file
+chmod 700 /home/test15
+chmod 644 /home/test15/secret_file
 
 #18
-echo "u1 ALL=(ALL) NOPASSWD: /usr/bin/passwd [A-Za-z0-9_-]*" | sudo EDITOR='tee -a' visudo
+echo "u1 ALL=(ALL) NOPASSWD: /usr/bin/passwd [A-Za-z0-9_-]*" > /etc/sudoers.d/u1
