d5af55bc3dde2d1641753c28d1829f6ad0b190ea
lilpipidron <lociX55lim@gmail.com>
2025-02-14 12:08:44 +0300

    feat: complete clear script

--- lab1/script2.sh
+++ lab1/script2.sh
@@ -13,3 +13,13 @@ sudo userdel u1
 sudo userdel u2
 
 sudo rm -rf /home/test13
+
+sudo groupdel test13_group
+
+sudo rm -f /etc/sudoers.d/u1
+
+sudo rm -rf /home/test13
+
+sudo rm -rf /home/test14
+
+sudo rm -rf /home/test15
